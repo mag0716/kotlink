bb68ef2a16d923a5ddeae9fb1a07cf8dad9a2ccc
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-11-03 22:03:22 +0900

    1つの suspend fun を実行するサンプル

--- CoroutinesExceptionHandling/app/src/main/java/com/github/mag0716/coroutinesexceptionhandling/MainActivity.kt
+++ CoroutinesExceptionHandling/app/src/main/java/com/github/mag0716/coroutinesexceptionhandling/MainActivity.kt
@@ -1,12 +1,78 @@
 package com.github.mag0716.coroutinesexceptionhandling
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    companion object {
+        private const val TAG = "CoroutinesExceptionHandling"
+    }
+
+    private lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        singleSuccessButton.setOnClickListener { singleApi(true) }
+        singleFailedButton.setOnClickListener { singleApi(false) }
+        singleWithContextSuccessButton.setOnClickListener { singleApiWithContext(true) }
+        singleWithContextFailedButton.setOnClickListener { singleApiWithContext(false) }
     }
+
+    override fun onResume() {
+        super.onResume()
+        job = Job()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        job.cancel()
+    }
+
+    private fun singleApi(isSuccess: Boolean) = launch {
+        try {
+            val data = fetchData(isSuccess)
+            Log.d(TAG, "singleApi : $data")
+        } catch (exception: Exception) {
+            Log.d(TAG, "singleApi: catch $exception")
+        }
+    }
+
+    private fun singleApiWithContext(isSuccess: Boolean) = launch {
+        try {
+            val data = fetchDataWithContext(isSuccess)
+            Log.d(TAG, "singleApiWithContext : $data")
+        } catch (exception: Exception) {
+            Log.d(TAG, "singleApiWithContext: catch $exception")
+        }
+    }
+
+    private suspend fun fetchData(isSuccess: Boolean): String {
+        Log.d(TAG, "fetchData : $isSuccess")
+        delay(3000)
+        if (isSuccess) {
+            return "Data"
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
+
+    private suspend fun fetchDataWithContext(isSuccess: Boolean): String =
+        withContext(Dispatchers.IO) {
+            Log.d(TAG, "fetchDataWithContext : $isSuccess")
+            delay(3000)
+            if (isSuccess) {
+                "Data"
+            } else {
+                throw IllegalArgumentException()
+            }
+        }
 }
